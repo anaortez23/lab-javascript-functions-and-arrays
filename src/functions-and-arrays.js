327909c504b63d85c9b0fac0736d6415fe8f494e
Alejandro Garcia <agarc371@gmail.com>
2020-06-08 20:04:09 -0400

    add code to call function in iteration 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,9 +1,11 @@
 // Iteration #1: Find the maximum
 
 function maxOfTwoNumbers(num1, num2){
-   if (num1 > num2)
-   return num1;
-   else return num2;
+   if (num1 === num2)
+    return num2;
+  else if(num1 > num2)
+    return num1;
+  else return num2;
 }
 
 maxofTwoNumbers(24, 42);
